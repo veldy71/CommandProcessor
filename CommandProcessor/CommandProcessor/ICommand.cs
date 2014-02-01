6e8404f907ca692c8349ae416715dc878c87566a
Thomas T Veldhouse <veldy@outlook.com>
2014-02-01 15:42:55 -0600

    Fixed generic constraints for ICommand

--- CommandProcessor/CommandProcessor/ICommand.cs
+++ CommandProcessor/CommandProcessor/ICommand.cs
@@ -4,7 +4,7 @@ namespace Veldy.Net.CommandProcessor
 {
     public interface ICommand<out TStore, TResponse> : IMessage<TStore>
         where TStore : struct, IConvertible
-        where TResponse : class, IResponse, IMessage
+        where TResponse : class, IResponse<TStore>, IMessage<TStore>
     {
          TResponse CreateResponse(TStore store);
     }
