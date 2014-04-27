db4dd2fb2f30460f813395b03a4be8cebd7cb45b
veldy71 <veldy@outlook.com>
2014-04-27 12:14:07 -0500

    Add nuget package.

--- CommandProcessor/Properties/AssemblyInfo.cs
+++ CommandProcessor/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CommandProcessor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CommandProcessor")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Thomas T. Veldhouse")]
+[assembly: AssemblyProduct("Veldy.Net.CommandProcessor")]
+[assembly: AssemblyCopyright("Copyright © Thomas T. Veldhouse 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
